9342c37795a50e8bd12991f4191658a8028e3618
Mantis Finance <hi@mantis.finance>
2021-02-12 16:39:33 +0800

    Get rid of docs folder

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const { emerald } = require('tailwindcss/colors');
 
 module.exports = {
   // `docs` is a special folder that can be served via Github Pages without using a `gh-pages` branch
-  outputDir: 'docs',
+  outputDir: 'dist',
   publicPath: '/',
   pwa: {
     themeColor: emerald['600'],
